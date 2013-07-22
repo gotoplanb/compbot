f313fa7287db7c481bc3b095d164c90ea6df2fa3
Dave Stanton <davestanton.us@gmail.com>
2013-07-21 22:10:51 -0400

    It works.

--- compbot.py
+++ compbot.py
@@ -2,22 +2,27 @@ from __future__ import print_function
 from wand.image import Image
 import glob
 
-# Get all PNGs in the current directory
-files = glob.glob('*.png')
-
 header_image = Image(filename='_header.png')
 footer_image = Image(filename='_footer.png')
 
-screen_file = files[2]
-screen_image = Image(filename=screen_file)
+# Get all PNGs in the current directory
+files = glob.glob('*.png')
 
-# Coordinates for where we composite
-comp_header = 0
-comp_footer = screen_image.height - footer_image.height
+# Pop the first two items because they are the _header and _footer
+files.pop(0)
+files.pop(0)
 
-# Do the compositing of header and footer onto the screen image
-screen_image.composite(header_image, 0, comp_header)
-screen_image.composite(footer_image, 0, comp_footer)
+for file in files:
 
-# Export appended image comp
-screen_image.save(filename=screen_file)
+	screen_image = Image(filename=file)
+
+	# Coordinates for where we composite
+	comp_header = 0
+	comp_footer = screen_image.height - footer_image.height
+
+	# Do the compositing of header and footer onto the screen image
+	screen_image.composite(header_image, 0, comp_header)
+	screen_image.composite(footer_image, 0, comp_footer)
+
+	# Export appended image comp
+	screen_image.save(filename=file)
