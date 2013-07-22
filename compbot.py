1de40190f22fb0514b763e8ea4806e74da24b4d6
Dave Stanton <davestanton.us@gmail.com>
2013-07-21 22:04:46 -0400

    Cleaning

--- compbot.py
+++ compbot.py
@@ -1,19 +1,23 @@
 from __future__ import print_function
 from wand.image import Image
+import glob
 
-header_file = 'header.png'
-footer_file = 'footer.png'
-screen_file = raw_input('What screen (png) to process?: ') + '.png'
+# Get all PNGs in the current directory
+files = glob.glob('*.png')
 
-header_image = Image(filename=header_file)
-footer_image = Image(filename=footer_file)
+header_image = Image(filename='_header.png')
+footer_image = Image(filename='_footer.png')
+
+screen_file = files[2]
 screen_image = Image(filename=screen_file)
 
+# Coordinates for where we composite
 comp_header = 0
 comp_footer = screen_image.height - footer_image.height
 
+# Do the compositing of header and footer onto the screen image
 screen_image.composite(header_image, 0, comp_header)
 screen_image.composite(footer_image, 0, comp_footer)
 
 # Export appended image comp
-screen_image.save(filename='comp.png')
+screen_image.save(filename=screen_file)
