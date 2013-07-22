878c8c0a9f2217c91950d494233f453118bd826b
Dave Stanton <davestanton.us@gmail.com>
2013-07-22 16:33:11 -0400

    Some refactoring and inital support for arbitrary number of partials.

--- compbot.py
+++ compbot.py
@@ -2,27 +2,40 @@ from __future__ import print_function
 from wand.image import Image
 import glob
 
-header_image = Image(filename='_header.png')
-footer_image = Image(filename='_footer.png')
+# Set all possible partials to a 1x1px transparent PNG.
+header_image = Image(filename='_empty.png')
+footer_image = Image(filename='_empty.png')
+leftrail = Image(filename='_empty.png')
+rightrail = Image(filename='_empty.png')
 
 # Get all PNGs in the current directory
 files = glob.glob('*.png')
 
-# Pop the first two items because they are the _header and _footer
-files.pop(0)
-files.pop(0)
+# Function used for printing the names of the partial files we found
+def partials_used(file):
+	print("Partial used: " + file)
 
 for file in files:
 
-	screen_image = Image(filename=file)
-
-	# Coordinates for where we composite
-	comp_header = 0
-	comp_footer = screen_image.height - footer_image.height
-
-	# Do the compositing of header and footer onto the screen image
-	screen_image.composite(header_image, 0, comp_header)
-	screen_image.composite(footer_image, 0, comp_footer)
-
-	# Export appended image comp
-	screen_image.save(filename=file)
+	# Check to see if the current file is a partial or a screen
+	if ( file == "_header.png" ):
+		header_image = Image(filename='_header.png')
+		partials_used(file)
+	elif ( file == "_footer.png" ):
+		footer_image = Image(filename='_footer.png')
+		partials_used(file)
+	elif ( file == "_leftrail.png" ):
+		partials_used(file)
+	elif ( file == "_rightrail.png" ):
+		partials_used(file)
+	else:
+		# Make a Wand object from our current file
+		screen_image = Image(filename=file)
+		# Coordinates for where we composite
+		comp_header = 0
+		comp_footer = screen_image.height - footer_image.height
+		# Do the compositing of header and footer onto the screen image
+		screen_image.composite(header_image, 0, comp_header)
+		screen_image.composite(footer_image, 0, comp_footer)
+		# Export appended image comp
+		screen_image.save(filename=file)
