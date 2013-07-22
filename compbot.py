360d063d5fa79cdeb93f476ba87be50a9b0904a6
Dave Stanton <davestanton.us@gmail.com>
2013-07-21 21:29:09 -0400

    Working.

--- compbot.py
+++ compbot.py
@@ -1,30 +1,19 @@
 from __future__ import print_function
 from wand.image import Image
-from wand.sequence import Sequence
 
 header_file = 'header.png'
 footer_file = 'footer.png'
-screen_file = raw_input('What screen to process?: ')
+screen_file = raw_input('What screen (png) to process?: ') + '.png'
 
 header_image = Image(filename=header_file)
 footer_image = Image(filename=footer_file)
 screen_image = Image(filename=screen_file)
 
-# Get the dimensions of the rectangle to keep
-crop_left = 0
-crop_top = header_image.height + 1
-crop_right = footer_image.width
-crop_bottom = screen_image.height - footer_image.height
+comp_header = 0
+comp_footer = screen_image.height - footer_image.height
 
-# Do the crop to keep area not covered by header.png or footer.png
-screen_image.crop(crop_left,crop_top,crop_right,crop_bottom)
-
-# Save the preserved area just for debugging
-screen_image.save(filename='middle.png')
-
-# Combine header, screen and footer into one image
-squish_image = Sequence(header_image)
-squish_image.append(screen_image)
+screen_image.composite(header_image, 0, comp_header)
+screen_image.composite(footer_image, 0, comp_footer)
 
 # Export appended image comp
-header_image.save(filename='squished.png')
+screen_image.save(filename='comp.png')
